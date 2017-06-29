f29933dda28355396e02e5f7574f0dfd251ac4fa
lapis-hong <dhq1993@qq.com>
2017-06-29 12:05:27 +0800

    add

--- prepareData/fm_index.py
+++ prepareData/fm_index.py
@@ -1,38 +1,58 @@
 import time
+import os
+import shutil
 data_libsvm_file = 'fmtrain20170403.txt'
 data_temp_file = 'fmtrain20170403.temp2'
 data_libfm_file = 'fmtrain20170403.libfm'
 
+TRAIN_FILE = 'train'
+TEST_FILE = 'test'
+DATA_DIR = 'data'
+
+# if exists dir, remove it, os.rmdir can only remove empty dir
+if os.path.exists(DATA_DIR):
+    shutil.rmtree(DATA_DIR)
+
+
+def gen_path(directory, filename):
+    return os.path.join(directory, filename)
+
+
+def make_path(name):
+    if os.path.exists(name):
+        os.rmdir(name)
+        os.mkdir(name)
+
 
 def convert_libfm(input_file, temp_file, output_file):
-    '''
-    :param input_file: the original libsvm format filepath
+    """
+    :param input_file: the original libsvm format file
     :param temp_file: reindex from zero
     :param output_file: the libfm format filepath
     :return: 
-    '''
+    """
     time_start = time.time()
-    with open(input_file) as data_libsvm, \
-        open(temp_file, 'w') as data_temp, \
-        open(output_file, 'w') as data_libfm:
+    with open(input_file) as data_libsvm,\
+            open(os.path.join(DATA_DIR, temp_file), 'w') as data_temp,\
+            open(os.path.join(DATA_DIR, output_file), 'w') as data_libfm:
         data_temp_list = []
         data_libfm_list = []
         count = 0
         for line in data_libsvm:
-            line = line.strip('\n').split(' ') # convert string to list
+            line = line.strip('\n').split(' ')  # convert string to list
             label = line.pop(0)
             data_temp_list.append(str(label))
             data_libfm_list.append(str(label))
 
-            index_all = [int(w.split(':')[0]) for w in line] # save all the original index values
-            index_set = list(set(index_all)) # unique original index values
+            index_all = [int(w.split(':')[0]) for w in line]  # save all the original index values
+            index_set = list(set(index_all))  # unique original index values
 
             for element in line:
                 temp = element.split(':')  # like ['5', '1']
-                feature_value = temp[-1] # string format
-                feature_index_new = index_set.index(int(temp[0])) # reindex from zero
+                feature_value = temp[-1]  # string format
+                feature_index_new = index_set.index(int(temp[0]))  # reindex from zero
                 data_temp_list.append(' ' + str(feature_index_new) + ':' + str(feature_value))
-                if feature_value != 0: # remove zero feature index and values
+                if feature_value != 0:  # remove zero feature index and values
                     data_libfm_list.append(' ' + str(feature_index_new) + ':' + str(feature_value))
             data_temp_list.append('\n')
             data_libfm_list.append('\n')
@@ -42,35 +62,32 @@ def convert_libfm(input_file, temp_file, output_file):
         data_temp.write(''.join(data_temp_list))
         data_libfm.write(''.join(data_libfm_list))
         time_end = time.time()
-        print('Successfully convert to the libfm format! Take %s sec:' %(time_end - time_start))
+        print('Successfully convert to the libfm format! Take %s sec:' % (time_end - time_start))
 
 
-def split_data(file_path, train_path, test_path, k=5):
-    '''
+def split_data(file_path, train_file, test_file, k=5):
+    """
     :param file_path: 
-    :param train_path: 
-    :param test_path: 
+    :param train_file: 
+    :param test_file: 
     :param k: 
     :return: 
-    '''
-    with open(train_path, 'w') as fm_train,\
-          open(test_path, 'w') as fm_test,\
-           open(file_path) as train_data:
+    """
+    with open(os.path.join(DATA_DIR, train_file), 'w') as fm_train, \
+            open(os.path.join(DATA_DIR, test_file), 'w') as fm_test, \
+            open(file_path) as train_data:
         line = train_data.readline()
         count = 1
         while line:
-            if count % k == 0: # chose 20% as test data
+            if count % k == 0:  # chose 20% as test data
                 fm_test.write(line)
             else:
                 fm_train.write(line)
             line = train_data.readline()
             count += 1
-    return train_path, test_path
+    return os.path.abspath(train_file), os.path.abspath(test_file)
 
 
 if __name__ == '__main__':
     convert_libfm(data_libsvm_file, data_temp_file, data_libfm_file)
-    split_data(data_libfm_file, 'train', 'test')
-
-
-
+    split_data(data_libfm_file, TRAIN_FILE, TEST_FILE)
--- train.py
+++ train.py
@@ -1,30 +1,34 @@
 from __future__ import print_function
-import os
 import pyFM
 from embedding import *
 from prepareData.fm_index import *
 from prepareData.embeddingIndexInit import *
 
-ORIGINAL_DATA_PATH = ['/home/apps/jarfile/lxt/libfm/trainAndpredict/prepareData/fmtrain20170403.libsvm',\
-                      '/home/apps/jarfile/lxt/libfm/trainAndpredict/prepareData/fmtrain20170404.libsvm',\
-                      '/home/apps/jarfile/lxt/libfm/trainAndpredict/prepareData/fmtrain20170404.libsvm' ]
-TRAIN_PATH = 'trainData' # or divide the data by dt
-TEST_PATH = 'testData'
+
+ORIGINAL_DATA_PATH = ['/home/apps/jarfile/lxt/libfm/trainAndpredict/prepareData/fmtrain20170403.libsvm',
+                      '/home/apps/jarfile/lxt/libfm/trainAndpredict/prepareData/fmtrain20170404.libsvm',
+                      '/home/apps/jarfile/lxt/libfm/trainAndpredict/prepareData/fmtrain20170404.libsvm']
+MODEL_DIR = 'model'
+MODEL_FILE = 'model'
+LATENT_FILE = 'latent'
+EMBEDDING_FILE = 'embeddingData'
+
+MODEL_PATH = gen_path(MODEL_DIR, MODEL_FILE)
+LATENT_PATH = gen_path(MODEL_DIR, MODEL_FILE)
+EMBEDDING_PATH = gen_path(MODEL_DIR, EMBEDDING_FILE)
 
 
 def make_path(name):
-    if  os.path.exists(name):
-        os.remove(name)
-    os.mkdir(name)
+    if os.path.exists(name):
+        os.rmdir(name)
+        os.mkdir(name)
+    return name
 
-MODEL_PATH = make_path('model')
-LATENT_PATH = make_path('latent')
-EMBEDDING_PATH = make_path('embeddingData')
 
 # trainData, testData = map(split_data, ORIGINAL_DATA_PATH, TRAIN_PATH*len(), TEST_PATH)
 for i in range(len(ORIGINAL_DATA_PATH)):
-    split_data(ORIGINAL_DATA_PATH[i], TRAIN_PATH, TEST_PATH)
-    print('%s split completed, writing train data into %s' %(ORIGINAL_DATA_PATH[i], TRAIN_PATH ))
+    split_data(ORIGINAL_DATA_PATH[i], TRAIN_FILE, TEST_FILE)
+    print('%s split completed, writing train data into %s' % (ORIGINAL_DATA_PATH[i], TRAIN_FILE))
 
 # set the parameter of FM
 fm = pyFM.FM(
@@ -34,16 +38,16 @@ fm = pyFM.FM(
     learning_method='sgd',
     learn_rate=0.01,
     r2_regularization=0.01,
-    temp_path=MODEL_PATH # save temp file, model path
+    temp_path=MODEL_PATH  # save temp file, model path
 )
 
 # run the fm model
 time_start = time.time()
 model = fm.run(
-    train_set = TRAIN_PATH,
-    test_set = TEST_PATH)
+    train_set=TRAIN_FILE,
+    test_set=TEST_FILE)
 time_end = time.time()
-print("FM training is finished. Taking %s sec." %(time_end-time_start))
+print("FM training is finished. Taking %s sec." % (time_end-time_start))
 
 # save the latent vectors
 latent = model.pairwise_interactions
@@ -51,11 +55,6 @@ save_data(latent, LATENT_PATH)
 print('Already write latent vectors into {}'.format(LATENT_PATH))
 
 index_list = embedding_index_init()
-new_data = latent_embedding(TRAIN_PATH, latent, index_list)
+new_data = latent_embedding(TRAIN_FILE, latent, index_list)
 save_data(new_data, EMBEDDING_PATH)
 print('Successfully saved embedding data on {}'.format(EMBEDDING_PATH))
-
-
-
-
-
